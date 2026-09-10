424ffc9fb305c7f87277d1096b5b003b84193e21
lac <hgiang16260@gmail.com>
2026-09-10 13:13:15 +0700

    file

--- Backend/testapi.py
+++ Backend/testapi.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Cho phép React gọi đến FastAPI
+origins = [
+    "http://localhost:5173",  # React Vite
+    "http://localhost:3000",  # React thường
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def home():
     return {
-        "message": "hello World"
+        "message": "Welcome to the Agri Audit Chain API!"
     }
